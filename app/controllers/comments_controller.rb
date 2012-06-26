bf23217a6884ba9cb3802a95290e849ed7f808f2
Spoorthy Ananthaiah <spoorthya@gmail.com>
2012-06-26 16:18:46 -0700

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  # GET /comments
+  # GET /comments.json
+
+  # POST /comments
+  # POST /comments.json
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment])
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myblog::Application.config.secret_token = '5e312127371be79c0e8fbeaed4d2c665e04c5326e358fb9da6b142291aff9e0896daaf9a2b45f2fee57dbf55b8bfa77f1bbc91c6af5f100b4d316d8386f3bb87'
